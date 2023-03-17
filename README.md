11512c7e8bcca307dbdb4e50c886626fc5cbe0b9
mazzzystar <2680461921@qq.com>
2023-03-17 16:20:45 +0800

    update README

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ https://podcastpulse.azurewebsites.net/
 A website that help you to know what podcasters think of hot & new things, **every line** of code in the project is written by gpt-4.
 
 ![](Assets/index.jpg)
+
+## Run
+```bash
+./generate.sh
+
+cd Web
+zip -r Web.zip  . -x '.??*'
+
+./deploy.sh
+```
--- Web/app.py
+++ Web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,12 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search', methods=['POST'])
-def search():
-    keyword = request.form.get('keyword')
-    # Perform search operation with the keyword
-    results = ['Result 1', 'Result 2', 'Result 3']  # Replace this with your search results
-    return render_template('results.html', results=results)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
