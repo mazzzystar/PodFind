3b78e497a41355ae4ff609c250e582e854d94dc4
mazzzystar <2680461921@qq.com>
2023-03-17 09:56:51 +0800

    remove deploy bash

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-# Change these values to the ones used to create the App Service.
-RESOURCE_GROUP_NAME='podcastpulse_group-8341'
-APP_SERVICE_NAME='podcastpulse'
-
-az webapp deploy \
-    --name $APP_SERVICE_NAME \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --src-path web.zip
--- generate.sh
+++
@@ -1,7 +0,0 @@
-RESOURCE_GROUP_NAME='podcastpulse_group-8341'
-APP_SERVICE_NAME='podcastpulse'
-
-az webapp config appsettings set \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --name $APP_SERVICE_NAME \
-    --settings SCM_DO_BUILD_DURING_DEPLOYMENT=true
