9f82053cde1077b6c359195a93e72dff56ac7f39
mazzzystar <2680461921@qq.com>
2023-03-27 12:21:43 +0800

    add about page.

--- Web/app.py
+++ Web/app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
